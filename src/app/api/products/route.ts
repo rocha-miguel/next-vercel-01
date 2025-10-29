e318b337be130b69c6e786bfd0dc603b50e49c2d
rocha-miguel <miguelrocha1818@gmail.com>
2025-10-29 20:28:15 -0300

    Meu Projeto

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,30 @@
+import { Product, products } from "@/lib/data";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json(products)
+
+
+}
+export async function POST(request: Request) {
+        const newProduct: Product = await request.json()
+
+        if (!newProduct?.title || !newProduct?.price) {
+            return NextResponse.json ({error: 'Dados incompletos'}, {status: 400})
+        }
+
+        const newId = products.length > 0 ? Math.max(...products.map((p) => {
+            if (p.id) {
+                return p.id
+            }
+            return 0
+            
+        })) + 1 : 1
+        newProduct.id = newId
+        products.push(newProduct)
+
+        return NextResponse.json(newProduct, {status: 201})
+    }
+
+
---
+++ src/lib/data.ts
@@ -0,0 +1,34 @@
+export interface Product {
+    id?: number,
+    title: string,
+    price: number
+}
+
+export const products: Product [] = [
+    {
+        id: 1,
+        title: 'Celular',
+        price: 1000
+    },
+    {
+        id: 2,
+        title: 'Tablet',
+        price: 1500.0,
+    
+    },
+    {
+        id: 3,
+        title: 'Notebook',
+        price: 25000
+    },
+    {
+        id: 5,
+        title: 'Notebook',
+        price: 25000
+    },
+    {
+        id: 4,
+        title:'Batata',
+        price: 10
+    }
+]
