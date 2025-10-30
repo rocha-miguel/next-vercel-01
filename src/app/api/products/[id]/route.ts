b0e1aebd2450413a77d5f4f04fa59808260df94a
rocha-miguel <miguelrocha1818@gmail.com>
2025-10-29 21:37:12 -0300

    Código atualizado

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,54 +1,39 @@
-import { NextResponse, NextRequest } from "next/server";
-import { products } from "@/lib/data";
+import { Product, products } from "@/lib/data";
+import { NextResponse } from "next/server";
 
-interface RouteParams {
-    params: {
-        id: string;
-    };
-}
+interface Params { id: string; }
 
-export async function GET(
-    request: NextRequest,
-    { params }: RouteParams
-) {
-    const id = parseInt(params.id);
-    const product = products.find((p) => p.id === id);
+export async function GET(request: Request, context: { params: Promise<Params> }) {
+    const params = await context.params
+    const id = parseInt(params.id)
+    const product = products.find((p) => p.id === id)
 
     if (!product) {
-        return NextResponse.json({ error: "Produto não encontrado" }, { status: 404 });
+        return NextResponse.json({ error: 'Produto nao encontrado' }, { status: 404 })
     }
 
-    return NextResponse.json(product);
+    return NextResponse.json(product)
 }
 
-export async function PUT(
-    request: NextRequest,
-    { params }: RouteParams
-) {
-    const id = parseInt(params.id);
-    const index = products.findIndex((p) => p.id === id);
-
+export async function PUT(request: Request, context: { params: Promise<Params> }) {
+    const params = await context.params
+    const id = parseInt(params.id)
+    const index = products.findIndex((p) => p.id === id)
     if (index < 0) {
-        return NextResponse.json({ error: "Produto não encontrado" }, { status: 404 });
+        return NextResponse.json({ error: 'Produto nao encontrado' }, { status: 404 })
     }
-
-    const updatedProduct = await request.json();
-    products[index] = { ...updatedProduct, id };
-
-    return NextResponse.json(products[index]);
+    const updatedProduct: Product = await request.json()
+    products[index] = { ...updatedProduct, id }
+    return NextResponse.json(products[index])
 }
 
-export async function DELETE(
-    request: NextRequest,
-    { params }: RouteParams
-) {
-    const id = parseInt(params.id);
-    const index = products.findIndex((p) => p.id === id);
-
+export async function DELETE(request: Request, context: { params: Promise<Params> }) {
+    const params = await context.params
+    const id = parseInt(params.id)
+    const index = products.findIndex((p) => p.id === id)
     if (index < 0) {
-        return NextResponse.json({ error: "Produto não encontrado" }, { status: 404 });
+        return NextResponse.json({ error: 'Produto nao encontrado' }, { status: 404 })
     }
-
-    products.splice(index, 1);
-    return NextResponse.json({ id });
+    products.splice(index, 1)
+    return NextResponse.json({ id })
 }
