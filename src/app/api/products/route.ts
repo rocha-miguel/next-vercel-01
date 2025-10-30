742c32a70d2f7393359dfcd6cfae69a67fb03d9d
rocha-miguel <miguelrocha1818@gmail.com>
2025-10-29 21:10:25 -0300

    Atualizando código

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { Product, products } from "@/lib/data";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 
 export async function GET() {
